ab01a0d2d86d7db04ec9e7d038747dde5af01f45
matthew@acrossthecloud.net <matthew@acrossthecloud.net>
2019-01-25 15:13:56 +1100

    kill unused function

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,31 +1,5 @@
 var countDownDate = new Date("May 7, 2019 00:00:00").getTime();
 
-// Update the count down every 1 second
-var countDown = setInterval(function() {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Output the result in an element with id="demo"
-    document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-
-    // If the count down is over, write some text
-    if (distance < 0) {
-        clearInterval(countDown);
-        document.getElementById("timer").innerHTML = "LIVE";
-    }
-}, 1000);
-
 $(document).ready(function(){
     var videoList = [
         "https://oceanarchive.io/media/Divers_01",
