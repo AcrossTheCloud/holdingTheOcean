350f226ab6ce4560b5ad0f26523600cf10c5b88d
Dan Wood <daniel@acrossthecloud.net>
2019-02-25 11:18:45 +1100

    Removed carousel and lightbox dependencies

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -20,33 +20,33 @@ $(document).ready(function(){
     $video[0].play();
 
 
-    let bgVideo = $('#bgVideo').get(0);
-    // Pause the background video whenever a lightbox has opened.
-    $(document).on('lity:open', function(event, instance) {
-        bgVideo.pause();
-    });
-    // Resume the background video whenever a lightbox has closed.
-    $(document).on('lity:close', function(event, instance) {
-        bgVideo.play();
-    });
+    // let bgVideo = $('#bgVideo').get(0);
+    // // Pause the background video whenever a lightbox has opened.
+    // $(document).on('lity:open', function(event, instance) {
+    //     bgVideo.pause();
+    // });
+    // // Resume the background video whenever a lightbox has closed.
+    // $(document).on('lity:close', function(event, instance) {
+    //     bgVideo.play();
+    // });
 
 
     // Setup lightbox click and populate it with a html5 video player.
-    $(document).on('click', '.lightboxVideo', function () {
-        var
-            videoURL = $(this).data('videourl');
-
-        lity('<video muted autoplay controls width="100%">\n' +
-            '    <source src="' + videoURL + '.mp4" type="video/mp4">\n' +
-            '    <source src="' + videoURL + '.webm" type="video/webm">\n' +
-            '</video>');
-    });
+    // $(document).on('click', '.lightboxVideo', function () {
+    //     var
+    //         videoURL = $(this).data('videourl');
+    //
+    //     lity('<video muted autoplay controls width="100%">\n' +
+    //         '    <source src="' + videoURL + '.mp4" type="video/mp4">\n' +
+    //         '    <source src="' + videoURL + '.webm" type="video/webm">\n' +
+    //         '</video>');
+    // });
 
     // Setup the carousel
-    var
-        $carousel = $('.owl-carousel'),
-        $carouselPrevButton = $('.carouselWrapper .control.prev'),
-        $carouselNextButton = $('.carouselWrapper .control.next');
+    // var
+    //     $carousel = $('.owl-carousel'),
+    //     $carouselPrevButton = $('.carouselWrapper .control.prev'),
+    //     $carouselNextButton = $('.carouselWrapper .control.next');
 
     // Show / hide the arrows if the item counts are lower/equal the item counts per window width
     // Only use this if we're not using loop: true on Owl.
@@ -67,37 +67,37 @@ $(document).ready(function(){
     //         $carouselNextButton.show();
     //     }
     // });
-
-    $carousel.owlCarousel({
-        margin: 10,
-        responsiveClass: true,
-        loop: true,
-
-        responsive: {
-            0: {
-                items: 1,
-                nav: false,
-                dots: false
-            },
-            600: {
-                items: 4,
-                nav: false,
-                dots: false
-            },
-            1000: {
-                items: 6,
-                nav: false,
-                dots: false
-            }
-        }
-    });
-
-    // Next and Prev buttons
-    $carouselPrevButton.click(function() {
-        $carousel.trigger('prev.owl.carousel');
-    });
-    $carouselNextButton.click(function() {
-        $carousel.trigger('next.owl.carousel');
-    });
+    //
+    // $carousel.owlCarousel({
+    //     margin: 10,
+    //     responsiveClass: true,
+    //     loop: true,
+    //
+    //     responsive: {
+    //         0: {
+    //             items: 1,
+    //             nav: false,
+    //             dots: false
+    //         },
+    //         600: {
+    //             items: 4,
+    //             nav: false,
+    //             dots: false
+    //         },
+    //         1000: {
+    //             items: 6,
+    //             nav: false,
+    //             dots: false
+    //         }
+    //     }
+    // });
+    //
+    // // Next and Prev buttons
+    // $carouselPrevButton.click(function() {
+    //     $carousel.trigger('prev.owl.carousel');
+    // });
+    // $carouselNextButton.click(function() {
+    //     $carousel.trigger('next.owl.carousel');
+    // });
 
 });
