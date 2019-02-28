dbb6867a8d6b8252b0df35135fc2d097dc236335
Dan Wood <daniel@acrossthecloud.net>
2019-03-01 09:36:41 +1100

    Unsubscribe and double optin screens

--- public/scripts/subscribe.js
+++ public/scripts/subscribe.js
@@ -5,8 +5,22 @@ var errorMessage = {
     network: 'Pps. network error, email us please.'
 };
 
-
 $(document).ready(function(){
+  // Check if we're opting into the mailing list
+  if(getUrlParameterName() === "key" && getUrlParameterValue('key')) {
+    openFullScreen('doubleOptIn');
+  }
+  // Check if we're unsubscribeing
+  if(getUrlParameterName() === "unsubscribe" || getUrlParameterName() === "email" || getUrlParameterValue('email')) {
+    openFullScreen('unsubscribe');
+    $('#unsubscribe input').val(getUrlParameterValue('email'));
+  }
+
+  $.ajaxSetup({
+    headers:{
+      "Content-Type": "application/json",
+    }
+  });
 
   $('#followForm').submit(function(e){
     e.preventDefault();
@@ -48,7 +62,120 @@ $(document).ready(function(){
 
 });
 
+function optIn(name, email, callback) {
+  $.ajax({
+    type:'POST',
+    url: 'https://ryuga4srzh.execute-api.ap-southeast-2.amazonaws.com/prod/optIn',
+    data: JSON.stringify({
+      name: name,
+      email: email
+    }),
+    contentType: 'application/json'
+  }).done(function(response) {
+    if(response.message === "OK") {
+      callback(true);
+    }
+  }).fail(function() {
+    callback(false);
+  });
+}
 
+
+function openFullScreen(id) {
+  var $element = $('#' + id);
+  $element.show();
+  $element.animate({'opacity' : 1}, 'slow', '', function () {
+    $(this).css('z-index', 999);
+    $('header').removeClass('open');
+  });
+}
+function closeFullScreen(id) {
+  var $element = $('#' + id);
+  $element.animate({'opacity' : 0}, 'slow','', function () {
+    $(this).css('z-index', 0);
+    $('header').removeClass('open');
+  });
+}
+
+function doubleOptIn() {
+  var hideContents = function(callback) {
+    $('#doubleOptIn h2').fadeOut();
+    $('#doubleOptIn .button.confirm').fadeOut(function () {
+      if (typeof callback === "function") { return callback(); }
+    });
+  };
+
+  hideContents(function () {
+    $.ajax({
+      type:'POST',
+      url: 'https://ryuga4srzh.execute-api.ap-southeast-2.amazonaws.com/prod/doubleOptIn?key=' + getUrlParameterValue('key'),
+      data: '',
+      contentType: 'application/json'
+    }).done(function(response) {
+      if(response.message === "OK") {
+        $('#doubleOptIn h2.title').html("You've successfully subscribed to our mailing list!").fadeIn();
+        $('#doubleOptIn .button.backto').fadeIn(5000);
+
+        // Remove the key from the URL
+        window.history.replaceState({}, document.title, "/");
+      }
+    }).fail(function() {
+      $('#doubleOptIn h2.error').fadeIn();
+
+      setTimeout(function () {
+        $('#doubleOptIn h2.error').fadeOut(function () {
+          $('#doubleOptIn h2.title').fadeIn();
+          $('#doubleOptIn .button.confirm').fadeIn();
+        });
+      },2000);
+    });
+  });
+}
+
+function unsubscribe() {
+  var hideContents = function(callback) {
+    $('#unsubscribe h2').fadeOut();
+    $('#unsubscribe input').fadeOut();
+    $('#unsubscribe .message span.error-message').fadeOut();
+    $('#unsubscribe .button').fadeOut(function () {
+      if (typeof callback === "function") { return callback(); }
+    });
+  };
+
+  var email = $('#unsubscribe input').val();
+
+  if(isBlank(email)) {
+    $('#unsubscribe .message span.error-message').text('Please enter your email address').fadeIn();
+    return;
+  }
+
+  if(!isEmail(email)) {
+    $('#unsubscribe .message span.error-message').text('Please enter a valid email address').fadeIn();
+    return;
+  }
+
+
+  hideContents(function () {
+    $.ajax({
+      type:'POST',
+      url: 'https://ryuga4srzh.execute-api.ap-southeast-2.amazonaws.com/prod/unsubscribe',
+      data: JSON.stringify({
+        'email': email
+      }),
+      contentType: 'application/json'
+    }).done(function(response) {
+      if(response.message === "OK") {
+        $('#unsubscribe h2.success').fadeIn(2000);
+        $('#unsubscribe .button.backto').fadeIn(5000);
+
+        // Remove the email param from the URL
+        window.history.replaceState({}, document.title, "/");
+      }
+    }).fail(function() {
+      $('#unsubscribe h2.error').fadeIn();
+    });
+  });
+}
 
 function postToMailer(body, url){
 
@@ -100,4 +227,15 @@ function isBlank(str) {
 function isEmail(email) {
   var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   return regex.test(email);
+}
+
+function getUrlParameterValue(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+function getUrlParameterName(name) {
+  var regex = new RegExp('^[\\?&]([a-zA-Z0-9_.+-]+)');
+  return regex.exec(location.search)[1];
 }
