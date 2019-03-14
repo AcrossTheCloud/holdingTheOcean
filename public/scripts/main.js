22d09f853f76b40fd16de1903ecfe0bbcbde2f70
Zara Glynn <zara.glynn.1992@gmail.com>
2019-03-14 16:10:05 +1100

    Twitch live stream in a lightbox

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,44 @@
+var liveStream = function() {
+    var autoplay = false;
+    if ($(window).width() > 480) {
+        autoplay = true;
+    }
+
+    return lity('<div class="iframe-container"> \
+        <svg class="closebtn" onclick="closeNav()" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 18 18"><line class="lines" x2="17.68" y2="17.68"></line><line class="lines" x1="17.68" y2="17.68"></line></svg> \
+        <iframe \
+        src="https://player.twitch.tv/?client-id=brdrlyou2po431ot4owmi1zzjn6n0x&channel=oceanspaceorg&muted=true&autoplay=' + autoplay + '"\
+        frameborder="0" \
+        scrolling="no" \
+        allowfullscreen="true"> \
+        </iframe></div>');
+}
+
 $(document).ready(function(){
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.twitch.tv/helix/streams?user_login=oceanspaceorg',
+        headers: {
+         "Accept":"application/vnd.twitchtv.v5+json",
+         "Client-ID":"brdrlyou2po431ot4owmi1zzjn6n0x"
+        }
+    })
+    .done( function(data) {
+        if (data.data.length > 0) {
+            var lity = liveStream();
+
+            var $liveStreamButton = $('#livestream');
+            $liveStreamButton.fadeIn();
+            $liveStreamButton.on('click', function () {
+                lity = liveStream();
+            });
+
+            $(document).on('click', '.lity-content .closebtn', function () {
+                lity.close();
+            });
+        }
+    });
+
     var videoList = [
         "https://oceanarchive.io/media/Divers_01",
         "https://oceanarchive.io/media/Turtle_Glowing+Fishies",
@@ -18,7 +58,6 @@ $(document).ready(function(){
 
     $video[0].load();
     $video[0].play();
-
 
     // let bgVideo = $('#bgVideo').get(0);
     // // Pause the background video whenever a lightbox has opened.
