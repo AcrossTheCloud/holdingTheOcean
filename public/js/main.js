97fb10d1835aa40ff90fdc0eb5c5e1cd6933b6b7
Dan Wood <daniel@acrossthecloud.net>
2019-06-07 14:48:44 +1000

    Forms

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,157 @@
+var headers = {
+  'Access-Control-Allow-Origin' : '*', // Required for CORS support to work
+  'Access-Control-Allow-Credentials' : true // Required for cookies, authorization headers with HTTPS
+};
+
+
 $(function() {
   OA_Logo_Lucid_Code();
-  loadVideos()
+  loadVideos();
+
+  var $collabForm = $('form#collab');
+  $collabForm.submit(function (e) {
+    e.preventDefault();
+    if($('#collab_agree').prop('checked')) {
+
+      var
+        $collab_name = $('#collab_name'),
+        $collab_name_value = $collab_name.val();
+      if (isBlank($collab_name.val())) {
+        $('#collab_name, label[for="collab_name"]').addClass('shake');
+        return;
+      }
+
+      var
+        $collab_email = $('#collab_email'),
+        $collab_email_value = $collab_email.val();
+      if (!isEmail($collab_email.val())) {
+        $('#collab_email, label[for="collab_email"]').addClass('shake');
+        return;
+      }
+
+      var
+        $collab_institution = $('#collab_institution'),
+        $collab_institution_value = $collab_institution.val();
+      if (isBlank($collab_institution.val())) {
+        $('#collab_institution, label[for="collab_institution"]').addClass('shake');
+        return;
+      }
+
+      var
+        $collab_location = $('#collab_location'),
+        $collab_location_value = $collab_location.val();
+      if (isBlank($collab_location.val())) {
+        $('#collab_location, label[for="collab_location"]').addClass('shake');
+        return;
+      }
+      var
+        $collab_message = $('#collab_message'),
+        $collab_message_value = $collab_message.val();
+      if (isBlank($collab_message.val())) {
+        $('#collab_message, label[for="collab_message"]').addClass('shake');
+        return;
+      }
+
+      var body = {
+        $collab_name_value,
+        $collab_email_value,
+        $collab_institution_value,
+        $collab_location_value,
+        $collab_message_value,
+        type: 'collaboration'
+      };
+
+      // Send form
+      $.ajax({
+        type:'POST',
+        url: 'https://api.ocean-archive.org/subscribe',
+        data: JSON.stringify(body),
+        contentType: 'application/json',
+        crossDomain: true,
+        headers: headers
+      }).done(function(data){
+        if(data.error){
+          $('#collabform .message p').html('Oops, looks like we\'ve had an issue!');
+        }
+      }).fail(function(data){
+        $('#collabform .message p').html('Oops, looks like we\'ve had an issue!');
+      });
+
+      $('form#collab').animate(
+        {
+          opacity: 0,
+          height: 0
+        },
+        2000,
+        function() {
+          $('#collabform .message').fadeIn('slow');
+        }
+      );
+
+    } else {
+      var $agreeBoxContainer = $collabForm.find('.custom-checkbox .col-12');
+      $agreeBoxContainer.addClass('shake');
+    }
+
+    setTimeout(function () {
+      $('.shake').removeClass('shake')
+    }, 1000);
+  });
+
+  var $aboutForm = $('form#aboutForm');
+  $aboutForm.submit(function (e) {
+    e.preventDefault();
+    if($('#collab_agree').prop('checked')) {
+
+      var
+        $about_email = $('#about_email'),
+        $about_email_value = $about_email.val();
+      if (!isEmail($about_email.val())) {
+        $('#about_email, label[for="about_email"]').addClass('shake');
+        return;
+      }
+
+      var body = {
+        $about_email_value,
+      };
+
+      // Send form
+      $.ajax({
+        type: 'POST',
+        url: 'https://api.ocean-archive.org/subscribe',
+        data: JSON.stringify(body),
+        contentType: 'application/json',
+        crossDomain: true,
+        headers: headers
+      }).done(function (data) {
+        if (data.error) {
+          $('#about .message p').html('Oops, looks like we\'ve had an issue!');
+        }
+      }).fail(function (data) {
+        $('#about .message p').html('Oops, looks like we\'ve had an issue!');
+      });
+
+      $('form#aboutForm, #about .receive').animate(
+        {
+          opacity: 0,
+          height: 0
+        },
+        2000,
+        function() {
+          $('#about .message').fadeIn('slow');
+        }
+      );
+
+    } else {
+      var $agreeBoxContainer = $collabForm.find('.custom-checkbox .col-12');
+      $agreeBoxContainer.addClass('shake');
+    }
+
+    setTimeout(function () {
+      $('.shake').removeClass('shake')
+    }, 1000);
+  });
+
 });
 
 
@@ -20,4 +171,13 @@ function loadVideos() {
     $video[0].load();
     $video[0].play();
   }
+}
+
+function isBlank(str) {
+  return (!str || /^\s*$/.test(str));
+}
+
+function isEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
 }
