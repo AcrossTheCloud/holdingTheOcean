4553a6779ec5b3d2daa9355f272f781cb9a09b9e
Dan Wood <daniel@acrossthecloud.net>
2019-06-08 10:51:36 +1000

    Form updates

--- public/js/main.js
+++ public/js/main.js
@@ -9,76 +9,61 @@ $(function() {
 
       var
         $collab_name = $('#collab_name'),
-        $collab_name_value = $collab_name.val();
-      if (isBlank($collab_name.val())) {
-        $('#collab_name, label[for="collab_name"]').addClass('shake');
-        return;
-      }
-
-      var
+        $collab_name_value = $collab_name.val(),
         $collab_email = $('#collab_email'),
-        $collab_email_value = $collab_email.val();
-      if (!isEmail($collab_email.val())) {
-        $('#collab_email, label[for="collab_email"]').addClass('shake');
-        return;
-      }
-
-      var
+        $collab_email_value = $collab_email.val(),
         $collab_institution = $('#collab_institution'),
-        $collab_institution_value = $collab_institution.val();
-      if (isBlank($collab_institution.val())) {
-        $('#collab_institution, label[for="collab_institution"]').addClass('shake');
-        return;
-      }
-
-      var
+        $collab_institution_value = $collab_institution.val(),
         $collab_location = $('#collab_location'),
-        $collab_location_value = $collab_location.val();
-      if (isBlank($collab_location.val())) {
-        $('#collab_location, label[for="collab_location"]').addClass('shake');
-        return;
-      }
-      var
+        $collab_location_value = $collab_location.val(),
         $collab_message = $('#collab_message'),
         $collab_message_value = $collab_message.val();
-      if (isBlank($collab_message.val())) {
+
+      if (isBlank($collab_name.val())) {
+        $('#collab_name, label[for="collab_name"]').addClass('shake');
+      } else if (!isEmail($collab_email.val())) {
+        $('#collab_email, label[for="collab_email"]').addClass('shake');
+      } else if (isBlank($collab_institution.val())) {
+        $('#collab_institution, label[for="collab_institution"]').addClass('shake');
+      } else if (isBlank($collab_location.val())) {
+        $('#collab_location, label[for="collab_location"]').addClass('shake');
+      } else if (isBlank($collab_message.val())) {
         $('#collab_message, label[for="collab_message"]').addClass('shake');
-        return;
-      }
+      } else {
+        var body = {
+          "name": $collab_name_value,
+          "email": $collab_email_value,
+          "location": $collab_location_value,
+          "institution": $collab_institution_value,
+          "message": $collab_message_value,
+          "type": "collaboration"
+        };
 
-      var body = {
-        "name": $collab_name_value,
-        "email": $collab_email_value,
-        "location": $collab_location_value,
-        "institution": $collab_institution_value,
-        "message": $collab_message_value,
-        "type": "collaboration"
-      };
-
-      // Send form
-      $.ajax({
-        type:'POST',
-        url: 'https://zey5d25xk6.execute-api.eu-central-1.amazonaws.com/dev/email',
-        data: JSON.stringify(body),
-        contentType: 'application/json'
-      }).done(function(data){
-        if(data.error){
+        // Send form
+        $.ajax({
+          type:'POST',
+          url: 'https://zey5d25xk6.execute-api.eu-central-1.amazonaws.com/dev/email',
+          data: JSON.stringify(body),
+          contentType: 'application/json'
+        }).done(function(data){
+          if(data.error){
+            $('#collabform .message p').html('Oops, looks like we\'ve had an issue!');
+          }
+        }).fail(function(data){
           $('#collabform .message p').html('Oops, looks like we\'ve had an issue!');
-        }
-      }).fail(function(data){
-        $('#collabform .message p').html('Oops, looks like we\'ve had an issue!');
-      });
+        });
 
-      $('form#collab').animate(
-        {
-          opacity: 0,
-          height: 0
-        },
-        2000,
-        function() {
-          $('#collabform .message').fadeIn('slow');
-        }
-      );
+        $('form#collab').animate(
+          {
+            opacity: 0,
+            height: 0
+          },
+          2000,
+          function() {
+            $('#collabform .message').fadeIn('slow');
+          }
+        );
+      }
 
     // } else {
     //   var $agreeBoxContainer = $collabForm.find('.custom-checkbox .col-12');
@@ -87,33 +72,27 @@ $(function() {
 
     setTimeout(function () {
       $('.shake').removeClass('shake')
-    }, 1000);
+    }, 1500);
   });
 
-  var $aboutForm = $('form#aboutForm');
-  $aboutForm.submit(function (e) {
+  $('form#aboutForm').submit(function (e) {
     e.preventDefault();
     // if($('#about_agree').prop('checked')) {
 
       var
         $about_email = $('#about_email'),
-        $about_email_value = $about_email.val(),
+        about_email_value = $about_email.val(),
         $about_name = $('#about_name'),
-        $about_name_value = $about_name.val();
+        about_name_value = $about_name.val();
 
-      if (!$about_name_value) {
-        $('#about_email, label[for="about_email"]').addClass('shake');
-        return;
-      }
-
-      if (!isEmail($about_email.val())) {
-        $('#about_email, label[for="about_email"]').addClass('shake');
-        return;
-      }
-
+    if (isBlank(about_name_value)) {
+      $('#about_email, label[for="about_name"]').addClass('shake');
+    } else if (!isEmail($about_email.val())) {
+      $('#about_email, label[for="about_email"]').addClass('shake');
+    } else {
       var body = {
-        "name": $about_name_value,
-        "email": $about_email_value,
+        "name": about_name_value,
+        "email": about_email_value,
         "type": "subscription"
       };
 
@@ -141,6 +120,65 @@ $(function() {
           $('#about .message').fadeIn('slow');
         }
       );
+    }
+
+    // } else {
+    //   var $agreeBoxContainer = $collabForm.find('.custom-checkbox .col-12');
+    //   $agreeBoxContainer.addClass('shake');
+    // }
+
+    setTimeout(function () {
+      $('.shake').removeClass('shake');
+
+    }, 1500);
+  });
+
+  $('form#aboutForm_1').submit(function (e) {
+    e.preventDefault();
+    // if($('#about_agree').prop('checked')) {
+
+      var
+        $about_email = $('#about_email_1'),
+        about_email_value = $about_email.val(),
+        $about_name = $('#about_name_1'),
+        about_name_value = $about_name.val();
+
+    if (isBlank(about_name_value)) {
+      $('#about_email_1, label[for="about_name_1"]').addClass('shake');
+    } else if (!isEmail($about_email.val())) {
+      $('#about_email_1, label[for="about_email_1"]').addClass('shake');
+    } else {
+      var body = {
+        "name": about_name_value,
+        "email": about_email_value,
+        "type": "subscription"
+      };
+
+      // Send form
+      $.ajax({
+        type: 'POST',
+        url: 'https://zey5d25xk6.execute-api.eu-central-1.amazonaws.com/dev/email',
+        data: JSON.stringify(body),
+        contentType: 'application/json',
+      }).done(function (data) {
+        if (data.error) {
+          $('#about .message p').html('Oops, looks like we\'ve had an issue!');
+        }
+      }).fail(function (data) {
+        $('#about .message p').html('Oops, looks like we\'ve had an issue!');
+      });
+
+      $('form#aboutForm_1, #about .receive').animate(
+        {
+          opacity: 0,
+          height: 0
+        },
+        2000,
+        function() {
+          $('#about .message').fadeIn('slow');
+        }
+      );
+    }
 
     // } else {
     //   var $agreeBoxContainer = $collabForm.find('.custom-checkbox .col-12');
@@ -149,7 +187,7 @@ $(function() {
 
     setTimeout(function () {
       $('.shake').removeClass('shake')
-    }, 1000);
+    }, 1500);
   });
 
 });
